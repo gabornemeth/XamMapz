925ff12b0201662a9c7e87990d82252ef9719ddf
Németh Gábor <gabor.nemeth1982@gmail.com>
2016-02-12 15:16:19 +0100

    add iOS sample

---
+++ Samples/XamMapz.Sample.iOS/AppDelegate.cs
@@ -0,0 +1,23 @@
+﻿using Foundation;
+using UIKit;
+
+namespace XamMapz.Sample.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            Xamarin.Forms.Forms.Init();
+            Xamarin.FormsMaps.Init();
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(application, launchOptions);
+        }
+    }
+}
+
+
